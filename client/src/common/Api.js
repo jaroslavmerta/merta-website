d4dccf798d38460fa469f7903ac5e75fb59590dd
jaroslavmerta <log.merta@gmail.com>
2022-01-20 14:16:27 +0100

    adjustment API_URL

--- client/src/common/Api.js
+++ client/src/common/Api.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const API_URL = 
-    `https://jaroslavmerta.herokuapp.com/${process.env.PORT}` ||
-    "http://localhost:5000"
-;
+const API_URL = `https://jaroslavmerta.herokuapp.com/${process.env.PORT}`;
 
 export const ApiGet = (url, par) => {
     return axios({ method: "get", url: API_URL + url, params: par })
