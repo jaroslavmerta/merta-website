6614229c54e575632ddbc975d9679063c8aa9fa9
jaroslavmerta <log.merta@gmail.com>
2022-01-20 18:25:54 +0100

    adds Procfile, deploy script and automatic code to set NODE_ENV

--- client/src/common/Api.js
+++ client/src/common/Api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import {config} from '../../../Constants'
 
-const API_URL = `https://jaroslavmerta.herokuapp.com`;
+const API_URL = config.url.API_URL;
 
 export const ApiGet = (url, par) => {
     return axios({ method: "get", url: API_URL + url, params: par })
