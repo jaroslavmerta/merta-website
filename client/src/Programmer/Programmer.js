4de535d4e85e636a8ae1dddb143ebcb288bec1e4
jaroslavmerta <log.merta@gmail.com>
2022-01-21 23:04:37 +0100

    adds SectionHeader in Programmer

--- client/src/Programmer/Programmer.js
+++ client/src/Programmer/Programmer.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import SectionHeader from './../Header/SectionHeader/SectionHeader';
 const Programmer = () => {
     return (
-        
-        <Link to={"/programmer/editor"} className="">
-             Programátorské portfolio
-        </Link>
+        <>
+            <SectionHeader
+            title='Programování' 
+            />
+
+            <Link to={"/programmer/editor"} className="">
+                Programátorské portfolio
+            </Link>
+        </>
     );
 }
 
